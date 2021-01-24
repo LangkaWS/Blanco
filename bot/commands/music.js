f9a4729087d3425510278abfa17663c98608d93d
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-01-24 22:17:06 +0100

    Update music.js
    - Import Tools.js
    - Rename execute -> play / play -> start
    - Rename server -> guild
    - Rename song -> music
    - Remove getArgs + isInVoiceChannel
    - Add notSameVoiceChannel
    - Add doc
    - Beautify code

--- bot/commands/music.js
+++ bot/commands/music.js
@@ -2,146 +2,213 @@ class Music {
     
     static ytdl   = require('ytdl-core');
     static config = require('../../config.json');
+    static Tools  = require('../Tools.js');
 
-    static prefix = this.config.PREFIX;
     static queue  = new Map();
 
     /**
-     * 
-     * @param {Message} message 
+     * Add the music to guild queue and start it if there is no current queue for the guild. 
+     * @param {Message} message the command message
      */
-    static async execute(message) {
-        if(!this.isInVoiceChannel(message.member)) {
-            return message.channel.send('Tu dois d\'abord te connecter à un canal vocal');
+    static async play(message) {
+        if(!message.member.voice.channel) {
+            message.channel.send('Tu dois d\'abord te connecter à un canal vocal');
+            return;
         }
 
         const voiceChannel = message.member.voice.channel;
-        const permissions = voiceChannel.permissionsFor(message.client.user);
+        const permissions  = voiceChannel.permissionsFor(message.client.user);
 
         if(!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-            return message.channel.send('J\'ai besoin des permissions de me connecter et de parler dans ton canal vocal pour jouer de la musique. Contactes un administrateur.');
+            message.channel.send('J\'ai besoin des permissions de me connecter et de parler dans ton canal vocal pour jouer de la musique. Contactes un administrateur.');
+            return;
         }
 
-        const songInfo = await this.ytdl.getInfo(this.getArgs(message)[0]).catch(e => this.sendError(e, message.channel));
-        const song = {
-            title: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url
+        const musicInfo = await this.ytdl.getInfo(this.Tools.getArgs(message)[0])
+                                         .catch(e => this.sendError(e, message.channel));
+        const music     = {
+            title: musicInfo.videoDetails.title,
+            url:   musicInfo.videoDetails.video_url
         };
+        const guildQueue = this.queue.get(message.guild.id);
 
-        const serverQueue = this.queue.get(message.guild.id);
-
-        if(!serverQueue) {
+        if(!guildQueue) {
             const queueConstruct = {
-                textChannel: message.channel,
+                textChannel:  message.channel,
                 voiceChannel: voiceChannel,
-                connection: null,
-                songs: [],
-                volume: 5,
-                playing: true
+                connection:   null,
+                musics:       [],
+                volume:       5,
+                playing:      true
             }
 
             this.queue.set(message.guild.id, queueConstruct);
-            queueConstruct.songs.push(song);
+            queueConstruct.musics.push(music);
 
             try {
-                var connection = await voiceChannel.join().catch(e => this.sendError(e, message.channel));
+                let connection            = await voiceChannel.join()
+                                                              .catch(e => this.sendError(e, message.channel));
                 queueConstruct.connection = connection;
-                this.play(message.guild.id, queueConstruct.songs[0]);
+                this.start(message.guild.id, queueConstruct.musics[0]);
             } catch (e) {
                 console.log(e);
                 this.queue.delete(message.guild.id);
-                return message.channel.send(e);
+                message.channel.send(e);
+                return;
             }
 
         } else {
-            serverQueue.songs.push(song);
-            message.channel.send(`${song.title} a été ajouté à la liste de lecture.`);
-            return message.channel.send(this.printQueue(serverQueue));
+            guildQueue.musics.push(music);
+            message.channel.send(`${music.title} a été ajouté à la liste de lecture.`);
+            message.channel.send(this.printQueue(guildQueue));
+            return;
         }  
     }
 
-    static play(guildId, song) {
-        const serverQueue = this.queue.get(guildId);
+    /**
+     * Play a music in the defined voice channel. If no music left, leave the channel and delete the guild queue.
+     * @param {number} guildId the id of the guild
+     * @param {object} music the music to play
+     */
+    static start(guildId, music) {
+        const guildQueue = this.queue.get(guildId);
 
-        if(!song) {
-            serverQueue.voiceChannel.leave();
+        if(!music) {
+            guildQueue.voiceChannel.leave();
             this.queue.delete(guildId);
-            return serverQueue.textChannel.send('Il n\' y a plus de musique en attente de lecture. Au revoir !');
+            guildQueue.textChannel.send('Il n\' y a plus de musique en attente de lecture. Au revoir !');
+            return;
         }
 
-        const dispatcher = serverQueue.connection.play(this.ytdl(song.url))
-                                                 .on('finish', () => {
-                                                     serverQueue.songs.shift();
-                                                     this.play(guildId, serverQueue.songs[0]);
-                                                 })
-                                                 .on('error', error => this.sendError(error, serverQueue.textChannel));
+        const dispatcher = guildQueue.connection.play(this.ytdl(music.url))
+                                                .on('finish', () => {
+                                                     guildQueue.musics.shift();
+                                                     this.start(guildId, guildQueue.musics[0]);
+                                                })
+                                                .on('error', error => this.sendError(error, guildQueue.textChannel));
         dispatcher.setVolumeLogarithmic(1);
-        serverQueue.textChannel.send(`Lecture en cours : **${song.title}**`);
-        serverQueue.textChannel.send(this.printQueue(serverQueue));
+        guildQueue.textChannel.send(`Lecture en cours : **${music.title}**`);
+        guildQueue.textChannel.send(this.printQueue(guildQueue));
     }
 
+    /**
+     * Skip the current music.
+     * @param {Message} message the command message
+     */
     static skip(message) {
-        if(this.isInVoiceChannel(message.member)) {
-            const serverQueue = this.queue.get(message.guild.id);
-            if(!serverQueue) {
-                return message.channel.send('Il n\'y a aucune musique en cours de lecture que je puisse passer.');
-            }
-            serverQueue.connection.dispatcher.end();
+        const memberVoiceChannel = message.member.voice.channel;
+        const guildQueue         = this.queue.get(message.guild.id);
+
+        if(memberVoiceChannel != guildQueue.voiceChannel) {
+            this.notSameVoiceChannel(message.channel);
+            return;
         }
+
+        if(!guildQueue) {
+            message.channel.send('Il n\'y a aucune musique en cours de lecture que je puisse passer.');
+            return;
+        }
+
+        guildQueue.connection.dispatcher.end();
     }
 
+    /**
+     * Stop the music and delete the guild queue.
+     * @param {Message} message the command message
+     */
     static stop(message) {
-        if(this.isInVoiceChannel(message.member)) {
-            const serverQueue = this.queue.get(message.guild.id);
-            if(!serverQueue) {
-                return message.channel.send('Il n\'y a aucune musique en cours de lecture.');
-            }
-            serverQueue.voiceChannel.leave();
-            this.queue.delete(message.guild.id);
-            return serverQueue.textChannel.send('Au revoir !');
+        const memberVoiceChannel = message.member.voice.channel;
+        const guildQueue         = this.queue.get(message.guild.id);
+
+        if(memberVoiceChannel != guildQueue.voiceChannel) {
+            this.notSameVoiceChannel(message.channel);
+            return;
         }
+
+        if(!guildQueue) {
+            message.channel.send('Il n\'y a aucune musique en cours de lecture.');
+            return;
+        }
+
+        guildQueue.voiceChannel.leave();
+        this.queue.delete(message.guild.id);
+        guildQueue.textChannel.send('Au revoir !');
     }
 
+    /**
+     * Pause current music.
+     * @param {Message} message the command message
+     */
     static pause(message) {
-        if(this.isInVoiceChannel(message.member)) {
-            const serverQueue = this.queue.get(message.guild.id);
-            if(!serverQueue) {
-                return message.channel.send('Il n\'y a aucune musique en cours de lecture.');
-            }
-            serverQueue.connection.dispatcher.pause();
+        const memberVoiceChannel = message.member.voice.channel;
+        const guildQueue         = this.queue.get(message.guild.id);
+
+        if(memberVoiceChannel != guildQueue.voiceChannel) {
+            this.notSameVoiceChannel(message.channel);
+            return;
         }
+
+        if(!guildQueue) {
+            message.channel.send('Il n\'y a aucune musique en cours de lecture.');
+            return;
+        }
+
+        guildQueue.connection.dispatcher.pause();
     }
 
+    /**
+     * Resume the current paused music.
+     * @param {Message} message the command message
+     */
     static resume(message) {
-        if(this.isInVoiceChannel(message.member)) {
-            const serverQueue = this.queue.get(message.guild.id);
-            if(!serverQueue || serverQueue.connection.dispatcher.paused == false) {
-                return message.channel.send('Il n\'y a aucune musique en pause.');
-            }
-            serverQueue.connection.dispatcher.resume();
+        const memberVoiceChannel = message.member.voice.channel;
+        const guildQueue         = this.queue.get(message.guild.id);
+
+        if(memberVoiceChannel != guildQueue.voiceChannel) {
+            this.notSameVoiceChannel(message.channel);
+            return;
         }
+
+        if(!guildQueue || guildQueue.connection.dispatcher.paused == false) {
+            message.channel.send('Il n\'y a aucune musique en pause.');
+            return;
+        }
+
+        guildQueue.connection.dispatcher.resume();
     }
 
-    static isInVoiceChannel(member) {
-        return member.voice.channel ? true : false;
-    }
-
-    static getArgs(message) {
-        return message.content.slice(this.prefix.length).split(' ').slice(1);
-    }
-
-    static printQueue(serverQueue) {
+    /**
+     * Take the guild queue and format it in user friendly playlist.
+     * @param {object} guildQueue 
+     * @returns {string} the playlist
+     */
+    static printQueue(guildQueue) {
         let str = '**__Liste de lecture :__**\n';
-        for(let i = 0; i < serverQueue.songs.length; i++) {
-            str += `${i == 0 ? '*(En cours) ' : '- '}` + serverQueue.songs[i].title + `${i == 0 ? '*' : ''}\n`;
+
+        for(let i = 0; i < guildQueue.musics.length; i++) {
+            str += `${i == 0 ? '*(En cours) ' : '- '}` + guildQueue.musics[i].title + `${i == 0 ? '*' : ''}\n`;
         }
+        
         return str;
     }
 
+    /**
+     * Prints the error in the console, delete the guild queue and send an error message in the channel.
+     * @param {object} error 
+     * @param {object} channel 
+     */
     static sendError(error, channel) {
         console.log(error);
         this.queue.delete(channel.guild.id);
-        return channel.send('J\'ai rencontré une erreur et dois partir.\n(Si l\'erreur persiste, contactez un administrateur.');
+        channel.send('J\'ai rencontré une erreur et dois partir.\n(Si l\'erreur persiste, contactez un administrateur.');
+    }
+
+    /**
+     * Called when the member and the bot need to be in the same voice channel, send a warning message.
+     * @param {TextChannel} textChannel the text channel to send the message
+     */
+    static notSameVoiceChannel(textChannel) {
+        textChannel.send('Tu dois être dans le même channel que moi pour exécuter cette action.');
     }
 }
 
