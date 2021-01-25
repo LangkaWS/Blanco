d7c778711d7e1135560906213dab7dd6507dd8c4
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-01-25 17:12:07 +0100

    Update Tools.js
    Update due to update in JSON property names

--- bot/Tools.js
+++ bot/Tools.js
@@ -1,7 +1,7 @@
 class Tools {
 
     static config = require('../config.json');
-    static prefix = this.config.PREFIX;
+    static prefix = this.config.prefix;
 
     static getArgs(message) {
         return message.content.slice(this.prefix.length).split(' ').slice(1);
