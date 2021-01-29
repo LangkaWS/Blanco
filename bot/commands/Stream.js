db0b9779a0a9f8a126db621957fc3a95b7c615d5
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-01-29 16:31:25 +0100

    Update Stream.js
    Remove class

--- bot/commands/Stream.js
+++ bot/commands/Stream.js
@@ -3,114 +3,107 @@ const Tools    = require('../Tools.js');
 
 const {StreamTxt, ErrorTxt, NotUnderstoodTxt} = require('../languages/fr.json');
 
-class Stream {
+exports.config = async (message) => {
+    try {
+        const guildId = message.guild.id;
+        const [result]  = await Database.getStreaming(guildId);
 
+        if(!result) {
+            message.channel.send(StreamTxt.NoConfig);
 
-    static async config(message) {
+            const streamRole    = await Tools.getReply(message, StreamTxt.AskRole);
+            const streamChannel = await Tools.getReply(message, StreamTxt.AskChannel);
+            const streamMessage = await Tools.getReply(message, StreamTxt.AskMessage);
 
-        try {
-            const guildId = message.guild.id;
-            const [result]  = await Database.getStreaming(guildId);
+            await Database.setStreaming(guildId, streamRole, streamChannel, streamMessage);
 
-            if(!result) {
-                message.channel.send(StreamTxt.NoConfig);
+            message.channel.send(StreamTxt.SuccessConfig);
 
-                const streamRole    = await Tools.getReply(message, StreamTxt.AskRole);
-                const streamChannel = await Tools.getReply(message, StreamTxt.AskChannel);
-                const streamMessage = await Tools.getReply(message, StreamTxt.AskMessage);
+        } else {
+            message.channel.send(StreamTxt.AlreadyConfig);
 
-                await Database.setStreaming(guildId, streamRole, streamChannel, streamMessage);
+            const currentRole      = result.streamingRole;
+            const currentChannelId = result.streamingChannelId;
+            const currentMessage   = result.streamingMessage;
 
-                message.channel.send(StreamTxt.SuccessConfig);
+            const channel = message.guild.channels.resolve(currentChannelId);
 
-            } else {
-                message.channel.send(StreamTxt.AlreadyConfig);
+            message.channel.send(StreamTxt.StreamingRole + currentRole + '\r'
+                + StreamTxt.StreamingChannel + (channel ? channel.name : StreamTxt.ChannelNotFound) + '\r'
+                + StreamTxt.StreamingMessage + currentMessage);
 
-                const currentRole      = result.streamingRole;
-                const currentChannelId = result.streamingChannelId;
-                const currentMessage   = result.streamingMessage;
+            let isReplyOk = false;
+            let endModify = false;
 
-                const channel = message.guild.channels.resolve(currentChannelId);
+            let newRole    = currentRole;
+            let newChannel = currentChannelId;
+            let newMessage = currentMessage;
 
-                message.channel.send(StreamTxt.StreamingRole + currentRole + '\r'
-                    + StreamTxt.StreamingChannel + (channel ? channel.name : StreamTxt.ChannelNotFound) + '\r'
-                    + StreamTxt.StreamingMessage + currentMessage);
+            do {
+                const reply = await Tools.getReply(message, StreamTxt.AskWhatToModify);
+                switch(reply) {
+                    case 'role':
+                        isReplyOk = true;
+                        newRole   = await Tools.getReply(message, StreamTxt.AskNewRole);
+                        break;
 
-                let isReplyOk = false;
-                let endModify = false;
+                    case 'channel':
+                        isReplyOk  = true;
+                        newChannel = await Tools.getReply(message, StreamTxt.AskNewChannel);
+                        break;
 
-                let newRole    = currentRole;
-                let newChannel = currentChannelId;
-                let newMessage = currentMessage;
+                    case 'message':
+                        isReplyOk  = true;
+                        newMessage = await Tools.getReply(message, StreamTxt.AskNewMessage);
+                        break;
 
-                do {
-                    const reply = await Tools.getReply(message, StreamTxt.AskWhatToModify);
-                    switch(reply) {
-                        case 'role':
-                            isReplyOk = true;
-                            newRole   = await Tools.getReply(message, StreamTxt.AskNewRole);
-                            break;
+                    case 'end':
+                        isReplyOk = true;
+                        endModify = true;
+                        await Database.updateStreaming(guildId, newRole, newChannel, newMessage);
+                        message.channel.send(StreamTxt.SuccessConfig);
+                        break;
 
-                        case 'channel':
-                            isReplyOk  = true;
-                            newChannel = await Tools.getReply(message, StreamTxt.AskNewChannel);
-                            break;
-
-                        case 'message':
-                            isReplyOk  = true;
-                            newMessage = await Tools.getReply(message, StreamTxt.AskNewMessage);
-                            break;
-
-                        case 'end':
-                            isReplyOk = true;
-                            endModify = true;
-                            await Database.updateStreaming(guildId, newRole, newChannel, newMessage);
-                            message.channel.send(StreamTxt.SuccessConfig);
-                            break;
-
-                        default:
-                            message.channel.send(NotUnderstoodTxt);
-                    }
-                } while(!isReplyOk || !endModify);
-            }
-
-        } catch (err) {
-            console.log(err);
-            message.channel.send(ErrorTxt);
+                    default:
+                        message.channel.send(NotUnderstoodTxt);
+                }
+            } while(!isReplyOk || !endModify);
         }
+
+    } catch (err) {
+        console.log(err);
+        message.channel.send(ErrorTxt);
     }
 
-    static async getStreamingRoleName(guildId) {
-        try {
-            const [result]  = await Database.getStreaming(guildId);
-            return result.streamingRole;
-        } catch (err) {
-            console.log(err);
-        }
-    }
+}
 
-    static async announceStream(member) {
-        try {
-            const guild = member.guild;
-            const [result]  = await Database.getStreaming(guild.id);
-            let message = result.streamingMessage;
-    
-            if(message.match(/{name}/)) {
-                message = message.replace(/{name}/, '**' + (member.nickname ? member.nickname : member.user.username) + '**');
-                console.log(message);
-            }
-    
-            if(message.match(/{url}/)) {
-                const url = member.presence.activities.find(act => act.type == 'STREAMING').url;
-                message = message.replace(/{url}/, url);
-            }
-    
-            const announcementChannel = guild.channels.resolve(result.streamingChannelId);
-            announcementChannel.send(message);
-        } catch (err) {
-            console.log(err);
-        }
+exports.getStreamingRoleName = async (guildId) => {
+    try {
+        const [result]  = await Database.getStreaming(guildId);
+        return result.streamingRole;
+    } catch (err) {
+        console.log(err);
     }
 }
 
-module.exports = Stream;
+exports.announceStream = async (member) => {
+    try {
+        const guild = member.guild;
+        const [result]  = await Database.getStreaming(guild.id);
+        let message = result.streamingMessage;
+
+        if(message.match(/{name}/)) {
+            message = message.replace(/{name}/, '**' + (member.nickname ? member.nickname : member.user.username) + '**');
+        }
+
+        if(message.match(/{url}/)) {
+            const url = member.presence.activities.find(act => act.type == 'STREAMING').url;
+            message = message.replace(/{url}/, url);
+        }
+
+        const announcementChannel = guild.channels.resolve(result.streamingChannelId);
+        announcementChannel.send(message);
+    } catch (err) {
+        console.log(err);
+    }
+}
