4fee273e28f59a0e4ecdfd987f05e61a7e322c7a
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-01-29 11:52:22 +0100

    Update Database.js
    Switch driver to mysql2 and use Promise Wrapper

--- bot/Database.js
+++ bot/Database.js
@@ -1,20 +1,14 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-exports.getConnection = () => {
-    
-    con.connect((err) => {
-        if(err) {
-            throw err;
-        }
-        console.log('Connected to database !');
-
+async function getConnection() {
+    return await mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     });
+}
+
+
 }
