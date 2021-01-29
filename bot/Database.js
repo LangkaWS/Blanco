e040738b7531dac1d067f41c02aa516ac629bd96
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-01-29 06:52:18 +0100

    Create Database.js

---
+++ bot/Database.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+exports.getConnection = () => {
+    
+    con.connect((err) => {
+        if(err) {
+            throw err;
+        }
+        console.log('Connected to database !');
+
+    });
+}
