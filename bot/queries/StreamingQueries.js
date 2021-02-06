3b9ffb634448d063ef9de2bf623feaa642c46ce6
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-02-06 19:42:27 +0100

    Add auto announcement setup in INSERT and UPDATE queries

--- bot/queries/StreamingQueries.js
+++ bot/queries/StreamingQueries.js
@@ -29,14 +29,14 @@ async function isStreamActive(guildId) {
     return rows;
 }
 
-async function setStreaming(guildId, role, channel, message) {
+async function setStreaming(guildId, role, channel, message, autoParam) {
     const con = await Database.getConnection();
-    await con.execute('INSERT INTO config SET guild_id = ?, str_channel_id = ?, str_role_id = ?, str_message = ?', [guildId, channel, role, message]);
+    await con.execute('INSERT INTO config SET guild_id = ?, str_channel_id = ?, str_role_id = ?, str_message = ?, str_active = ?', [guildId, channel, role, message, autoParam]);
 }
 
-async function updateStreaming(guildId, role, channel, message) {
+async function updateStreaming(guildId, role, channel, message, autoParam) {
     const con = await Database.getConnection();
-    await con.execute('UPDATE config SET str_channel_id = ?, str_role_id = ?, str_message = ? WHERE guild_id = ?', [channel, role, message, guildId]);
+    await con.execute('UPDATE config SET str_channel_id = ?, str_role_id = ?, str_message = ?, str_active = ? WHERE guild_id = ?', [channel, role, message, autoParam, guildId]);
 }
 
-module.exports = { getStreamingChannelAndMessage, getStreamingRoleId, isStreamActive, setStreaming, updateStreaming };
+module.exports = { getStreaming, getStreamingChannelAndMessage, getStreamingRoleId, toogleAutoAnnouncement, isStreamActive, setStreaming, updateStreaming };
