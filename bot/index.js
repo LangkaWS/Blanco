ace0b685fb8d16c1f6ffbebf90e1d5dd55210951
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-02-06 19:10:55 +0100

    Add a check if auto stream feature is active

--- bot/index.js
+++ bot/index.js
@@ -77,9 +77,12 @@ client.on('guildMemberUpdate', async (oldMember, newMember) => {
     try {
         const diffRole = newMember.roles.cache.difference(oldMember.roles.cache).first();
         if(diffRole) {
-            const streamingRole = await Stream.getStreamingRoleName(oldMember.guild.id);
-            if(streamingRole && diffRole.name === streamingRole && newMember.roles.cache.get(diffRole.id)) {
-                Stream.announceStream(newMember);
+            const streamingRole = await Stream.getStreamingRole(oldMember.guild);
+            if(streamingRole && diffRole === streamingRole && newMember.roles.cache.get(diffRole.id)) {
+                const isStreamActive = await Stream.isStreamActive(newMember.guild);
+                if(isStreamActive) {
+                    Stream.announceStream(newMember);
+                }
             }
         }
     } catch (error) {
