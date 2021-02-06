ef256ccd240bb5fc026696487d46d5bd4ba27678
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-02-06 14:51:46 +0100

    Add spacing

--- bot/Tools.js
+++ bot/Tools.js
@@ -1,4 +1,4 @@
-const { prefix } = require('../config.json');
+const { prefix }   = require('../config.json');
 const { ErrorTxt } = require('./languages/fr.json');
 
 async function getReply(initMessage, question) {
