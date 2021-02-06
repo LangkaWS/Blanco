ef0a3eebb12b535d5e36ddc88106476bd0fbd1b4
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-02-06 13:37:57 +0100

    End admin feature and add spacing

--- bot/commands/Setup.js
+++ bot/commands/Setup.js
@@ -33,64 +33,68 @@ async function setupAdmin(message) {
         const adminRoles = [];
         adminRolesConfig.forEach(role => adminRoles.push(role.role_id));
 
-        if(!adminRolesConfig.length) {
+        let isNewAdminSetup = false;
+
+        if (!adminRolesConfig.length) {
+            isNewAdminSetup = true;
+
             let wantCreateAdminRoles =  await Tools.getReply(message, Setup.AskSetupAdminRole);
-            while(wantCreateAdminRoles !== 'yes' && wantCreateAdminRoles !== 'no') {
+            while (wantCreateAdminRoles !== 'yes' && wantCreateAdminRoles !== 'no') {
                 wantCreateAdminRoles = await Tools.getReply(message, NotUnderstoodTxt);
             }
-            if(wantCreateAdminRoles === 'yes') {
+
+            if (wantCreateAdminRoles === 'yes') {
                 let reply;
                 do {
                     const roleMention = await Tools.getReply(message, Setup.AskAdminRoleToAdd);
                     reply = await addRole(message, roleMention, adminRoles);
-                } while(reply !== '!blanco next')
+                } while (reply !== '!blanco next')
             }
+
+            message.channel.send(Setup.AdminSetupEnd);
         }
         
-        if(isAdmin(message.member, adminRoles)) {
-            console.log('is admin');
+        if (isAdmin(message.member, adminRoles)) {
+            if (isNewAdminSetup === false) {
 
-            let wantModifyAdminRoles =  await Tools.getReply(message, Setup.AskModifyAdminRoles);
-            while(wantModifyAdminRoles !== 'yes' && wantModifyAdminRoles !== 'no') {
-                wantModifyAdminRoles = await Tools.getReply(message, NotUnderstoodTxt);
+                let wantModifyAdminRoles =  await Tools.getReply(message, Setup.AskModifyAdminRoles);
+                while (wantModifyAdminRoles !== 'yes' && wantModifyAdminRoles !== 'no') {
+                    wantModifyAdminRoles = await Tools.getReply(message, NotUnderstoodTxt);
+                }
+    
+                if (wantModifyAdminRoles === 'yes') {
+
+                    message.channel.send(Setup.AdminRolesList);
+                    let roleList = '';
+                    adminRoles.forEach(roleId => {
+                        let role = message.guild.roles.resolve(roleId);
+                        roleList += '- ' + role.name + '\r';
+                    });
+                    message.channel.send(roleList);
+    
+                    message.channel.send(Setup.AskAdminRolesModification);
+    
+                    let modificationCommand = '';
+    
+                    do {
+                        modificationCommand = await message.channel.awaitMessages(msg => msg.author.id === message.author.id, {max: 1});
+                        const [command, role] = Tools.getArgs(modificationCommand.first());
+                        if (command === 'add') {
+                            await addRole(message, role, adminRoles);
+                        } else if (command === 'remove') {
+                            await removeRole(message, role, adminRoles);
+                        } else if (command !== 'next') {
+                            message.channel.send(NotUnderstoodTxt);
+                        }
+                    } while (modificationCommand.first().content !== '!blanco next')
+                }
+
+                message.channel.send(Setup.AdminSetupEnd);
             }
-
-            if(wantModifyAdminRoles === 'yes') {
-                message.channel.send(Setup.AdminRolesList);
-                let roleList = '';
-                adminRoles.forEach(roleId => {
-                    let role = message.guild.roles.resolve(roleId);
-                    roleList += '- ' + role.name + '\r';
-                });
-                message.channel.send(roleList);
-
-                message.channel.send(Setup.AskAdminRolesModification);
-
-                let modificationCommand = '';
-
-                do {
-                    modificationCommand = await message.channel.awaitMessages(msg => msg.author.id === message.author.id, {max: 1});
-                    const [command, role] = Tools.getArgs(modificationCommand.first());
-                    if(command === 'add') {
-                        await addRole(message, role, adminRoles);
-                    } else if(command === 'remove') {
-                        await removeRole(message, role, adminRoles);
-                    } else if(command !== 'next') {
-                        message.channel.send(NotUnderstoodTxt);
-                    }
-                } while(modificationCommand !== '!blanco next')
-            }
-
-            //Ask want to create or modify config
-
-            const config = await Database.getSetup(message.guild.id);
-            console.log('config: ' + config);
-            console.log('Il y a déjà une configuration pour ce serveur : ');
         } else {
             message.channel.send(AccessDenied);
             return;
         }
-        
     } catch (err) {
         console.error(new Date());
         console.error(err);
@@ -99,8 +103,8 @@ async function setupAdmin(message) {
 
 async function addRole(message, roleMention, roles) {
     const roleId = roleMention.replace('<@&', '').replace('>', '');
-    if(!roles.includes(roleId)) {
-        if(isRole(message.guild, roleId)) {
+    if (!roles.includes(roleId)) {
+        if (isRole(message.guild, roleId)) {
             await AdminQueries.addAdminRole(message.guild.id, roleId);
             roles.push(roleId);
             message.channel.send(Setup.RoleSuccessfullyAdded);
@@ -115,8 +119,8 @@ async function addRole(message, roleMention, roles) {
 
 async function removeRole(message, roleMention, roles) {
     const roleId = roleMention.replace('<@&', '').replace('>', '');
-    if(roles.includes(roleId)) {
-        if(isRole(message.guild, roleId)) {
+    if (roles.includes(roleId)) {
+        if (isRole(message.guild, roleId)) {
             await AdminQueries.removeAdminRole(message.guild.id, roleId);
             roles.splice(roles.indexOf(roleId), 1);
             message.channel.send(Setup.RoleSuccessfullyRemoved);
