dae800a88b07d4f86fea3766dcd2274b706e5115
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-02-06 18:33:00 +0100

    Update StreamingQueries.js

--- bot/queries/StreamingQueries.js
+++ bot/queries/StreamingQueries.js
@@ -1,19 +1,19 @@
 const Database = require('./GlobalQueries.js');
 
 async function getStreaming(guildId) {
-    const con = await Database.getConnection();
-    const [rows] =  await con.execute('SELECT * FROM STREAMING_CONFIG WHERE guildID = ?', [guildId]);
+    const con    = await Database.getConnection();
+    const [rows] =  await con.execute('SELECT str_channel_id, str_role_id, str_message, str_active FROM config WHERE guild_id = ?', [guildId]);
     return rows;
 }
 
 async function setStreaming(guildId, role, channel, message) {
     const con = await Database.getConnection();
-    await con.execute('INSERT INTO STREAMING_CONFIG SET guildID = ?, streamingRole = ?, streamingChannelId = ?, streamingMessage = ?', [guildId, role, channel, message]);
+    await con.execute('INSERT INTO config SET guild_id = ?, str_channel_id = ?, str_role_id = ?, str_message = ?', [guildId, channel, role, message]);
 }
 
 async function updateStreaming(guildId, role, channel, message) {
     const con = await Database.getConnection();
-    await con.execute('UPDATE STREAMING_CONFIG SET streamingRole = ?, streamingChannelId = ?, streamingMessage = ? WHERE guildID = ?', [role, channel, message, guildId]);
+    await con.execute('UPDATE config SET str_channel_id = ?, str_role_id = ?, str_message = ? WHERE guild_id = ?', [channel, role, message, guildId]);
 }
 
 module.exports = { getStreaming, setStreaming, updateStreaming };
