f1f1777ac4e73bd31b8a1ee1b7fc743f23f0b048
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-02-06 19:11:56 +0100

    Add getStreamingChannelAndMessage and update exports

--- bot/queries/StreamingQueries.js
+++ bot/queries/StreamingQueries.js
@@ -5,6 +5,16 @@ async function getStreamingRoleId(guildId) {
     const [rows] = await con.execute('SELECT str_role_id FROM config WHERE guild_id = ?', [guildId]);
     return rows;
 }
+
+async function getStreamingChannelAndMessage(guildId) {
+    const con    = await Database.getConnection();
+    const [rows] = await con.execute('SELECT str_channel_id, str_message FROM config WHERE guild_id = ?', [guildId]);
+    return rows;
+}
+
+async function isStreamActive(guildId) {
+    const con    = await Database.getConnection();
+    const [rows] = await con.execute('SELECT str_active FROM config WHERE guild_id = ?', [guildId]);
     return rows;
 }
 
@@ -18,4 +28,4 @@ async function updateStreaming(guildId, role, channel, message) {
     await con.execute('UPDATE config SET str_channel_id = ?, str_role_id = ?, str_message = ? WHERE guild_id = ?', [channel, role, message, guildId]);
 }
 
-module.exports = { getStreaming, setStreaming, updateStreaming };
+module.exports = { getStreamingChannelAndMessage, getStreamingRoleId, isStreamActive, setStreaming, updateStreaming };
