4a14011350c1fdd36d7262aeffed7d1c43dbaf2d
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-02-08 12:50:51 +0100

    Add try...catch and con.end

--- bot/queries/AdminQueries.js
+++ bot/queries/AdminQueries.js
@@ -1,14 +1,30 @@
 const Database = require('./GlobalQueries.js');
 
 async function addAdminRole(guildId, roleId) {
-    const con    = await Database.getConnection();
-    const [rows] = await con.execute('INSERT INTO admin_roles SET guild_id = ?, role_id = ?', [guildId, roleId]);
-    return rows;
+    let con = null;
+    try {
+        con          = await Database.getConnection();
+        const [rows] = await con.execute('INSERT INTO admin_roles SET guild_id = ?, role_id = ?', [guildId, roleId]);
+        return rows;
+    } catch (err) {
+        console.log(err);
+        throw 'SQL Exception';
+    } finally {
+        con.end();
+    }
 }
 
 async function removeAdminRole(guildId, roleId) {
-    const con = await Database.getConnection();
-    await con.execute('DELETE FROM admin_roles WHERE guild_id = ? AND role_id = ?', [guildId, roleId]);
+    let con = null;
+    try {
+        con = await Database.getConnection();
+        await con.execute('DELETE FROM admin_roles WHERE guild_id = ? AND role_id = ?', [guildId, roleId]);
+    } catch (err) {
+        console.log(err);
+        throw 'SQL Exception';
+    } finally {
+        con.end();
+    }
 }
 
 module.exports = { addAdminRole, removeAdminRole };
