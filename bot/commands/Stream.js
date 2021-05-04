3a45bad10f957ddd849688c2e9ff73de20e24d1a
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-05-04 06:53:18 +0200

    Move stream setup from Tools to Stream

--- bot/commands/Stream.js
+++ bot/commands/Stream.js
@@ -1,7 +1,7 @@
 const StreamingQueries = require('../queries/StreamQueries.js');
 const Tools = require('../Tools.js');
 
-const { StreamTxt } = require('../languages/fr.json');
+const { StreamTxt, AccessDenied, NotUnderstoodTxt, RoleNotFound, SuccessConfig } = require('../languages/fr.json');
 
 /**
  * Call the appropriate function according to arguments of the command.
@@ -13,7 +13,7 @@ function menu(message) {
 
   switch (command) {
   case 'setup':
-    Tools.setup(feature, message);
+    setup(message);
     break;
 
   case 'next':
@@ -33,9 +33,115 @@ function menu(message) {
   }
 }
 
-async function getStreamingRole(guild) {
+async function setup(message) {
   try {
-    const [result] = await StreamingQueries.getStreamingRoleId(guild.id);
+
+    const isAdmin = await Tools.checkAdmin(message.member);
+    if (!isAdmin) return message.channel.send(AccessDenied);
+
+    const guild = message.guild;
+
+    const [setup] = await StreamingQueries.getSetup(message.guild.id);
+
+    if (setup) {
+
+      const announcementChannel = guild.channels.resolve(setup.channel_id);
+      const streamerRole = guild.roles.resolve(setup.streamer_role_id);
+      const liveRole = guild.roles.resolve(setup.live_role_id);
+      const announcementMessage = setup.message;
+
+      const currentSetupMessage = StreamTxt.AlreadySetup
+        + StreamTxt.StreamerRole + (streamerRole ? '<@&' + streamerRole + '>' : '') + '\r'
+        + StreamTxt.LiveRole + (streamerRole ? '<@&' + liveRole + '>' : '') + '\r'
+        + StreamTxt.Channel + (announcementChannel ? '<#' + announcementChannel + '>' : '') + '\r'
+        + StreamTxt.Message + announcementMessage;
+
+      message.channel.send(currentSetupMessage);
+    
+    }
+
+    let wantToSetup = await Tools.getReply(message, setup ? StreamTxt.AskModifyCurrentSetup : StreamTxt.NoSetup);
+
+    while (wantToSetup !== 'yes' && wantToSetup !== 'no') {
+      wantToSetup = await Tools.getReply(message, NotUnderstoodTxt);
+    }
+
+    if (wantToSetup === 'no') return;
+
+    //Setup new streamer role
+    let newStreamerRole = await Tools.getReply(message, (setup && setup.streamer_role_id) ? StreamTxt.AskModifyStreamerRole : StreamTxt.AskStreamerRole);
+    let newStreamerRoleId = setup.streamer_role_id;
+
+    if (newStreamerRole !== '!str next') {
+      newStreamerRoleId = newStreamerRole.replace('<@&', '').replace('>', '');
+      let isRole = Tools.checkRole(guild, newStreamerRoleId);
+
+      while (!isRole) {
+        newStreamerRole = await Tools.getReply(message, RoleNotFound);
+        newStreamerRoleId = newStreamerRole.replace('<@&', '').replace('>', '');
+        isRole = Tools.checkRole(guild, newStreamerRoleId);
+      }
+    }
+
+    //Setup new live role
+    let newLiveRole = await Tools.getReply(message, (setup && setup.live_role_id) ? StreamTxt.AskModifyLiveRole : StreamTxt.AskLiveRole);
+    let newLiveRoleId = setup.live_role_id;
+
+    if (newLiveRole !== '!str next') {
+      newLiveRoleId = newLiveRole.replace('<@&', '').replace('>', '');
+      let isRole = Tools.checkRole(guild, newLiveRoleId);
+
+      while (!isRole) {
+        newLiveRole = await Tools.getReply(message, RoleNotFound);
+        newLiveRoleId = newLiveRole.replace('<@&', '').replace('>', '');
+        isRole = Tools.checkRole(guild, newLiveRoleId);
+      }
+    }
+
+    //Setup new announcement channel
+    let newAnnouncementChannel = await Tools.getReply(message, (setup && setup.channel_id) ? StreamTxt.AskModifyChannel : StreamTxt.AskChannel);
+    let newAnnouncementChannelId = setup.channel_id;
+
+    if (newAnnouncementChannel !== '!str next') {
+      newAnnouncementChannelId = newAnnouncementChannel.replace('<#', '').replace('>', '');
+      let isChannel = Tools.checkChannel(guild, newAnnouncementChannelId);
+
+      while (!isChannel) {
+        newAnnouncementChannel = await Tools.getReply(message, RoleNotFound);
+        newAnnouncementChannelId = newAnnouncementChannel.replace('<#', '').replace('>', '');
+        isChannel = Tools.checkChannel(guild, newAnnouncementChannelId);
+      }
+    }
+
+    //Setup new announcement message
+    const reply = await Tools.getReply(message, (setup && setup.message) ? StreamTxt.AskModifyMessage : StreamTxt.AskMessage);
+    let newAnnouncementMessage = setup.message;
+
+    if (reply !== '!str next') {
+      newAnnouncementMessage = reply
+    }
+
+    //Setup auto announcement
+    let wantToEnableAutoAnnouncement = await Tools.getReply(message, StreamTxt.AskEnableAuto);
+
+    while (wantToEnableAutoAnnouncement !== 'yes' && wantToEnableAutoAnnouncement !== 'no') {
+      wantToEnableAutoAnnouncement = await Tools.getReply(message, NotUnderstoodTxt);
+    }
+    const auto = wantToEnableAutoAnnouncement === 'yes' ? 1 : 0;
+
+    if (setup) {
+      await StreamingQueries.updateSetup(guild.id, newStreamerRoleId, newLiveRoleId, newAnnouncementChannelId, newAnnouncementMessage, auto);
+    } else {
+      await StreamingQueries.createSetup(guild.id, newStreamerRoleId, newLiveRoleId, newAnnouncementChannelId, newAnnouncementMessage, auto);
+    }
+
+    message.channel.send(SuccessConfig);
+
+
+  } catch (error) {
+    Tools.sendError(error, message.channel);
+  }
+}
 
 async function getStreamerRole(guild) {
   try {
